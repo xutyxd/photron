98bc9cacc2beb68e8d4d65d9f9ff453f992d0b71
Xuty <xutyxd@gmail.com>
2024-10-03 23:01:03 +0200

    feat(folder:test:units): implemented unit test for file controller trying tdd

--- tests/units/file/controllers/file.controller.spec.ts
+++ tests/units/file/controllers/file.controller.spec.ts
@@ -5,47 +5,47 @@ import { beforeEach, describe, it } from "node:test";
 
 import { BadRequestResponse, NotFoundResponse } from "../../../../src/server/crosscutting/common/responses";
 import { MemoryDatabaseService } from "../../../../src/server/crosscutting/database/services/memory-database.service";
-import { FolderController } from "../../../../src/server/folder/controllers/folder.controller";
-import { IFolderAPI } from "../../../../src/server/folder/interfaces/folder-api.interface";
-import { IFolderModel } from "../../../../src/server/folder/interfaces/folder-model.interface";
-import { FolderRepository } from "../../../../src/server/folder/repository/folder.repository";
-import { FolderService } from "../../../../src/server/folder/services/folder.service";
+import { FileController } from "../../../../src/server/file/controllers/file.controller";
+import { IFileAPI } from "../../../../src/server/file/interfaces/file-api.interface";
+import { IFileModel } from "../../../../src/server/file/interfaces/file-model.interface";
+import { FileRepository } from "../../../../src/server/file/repository/file.repository";
+import { FileService } from "../../../../src/server/file/services/file.service";
 
-describe('FolderController', () => {
+describe('FileController', () => {
 
-    describe('FolderController constructor', () => {
+    describe('FileController constructor', () => {
         it('it should instance', () => {
-            let instance: FolderController | Error;
+            let instance: FileController | Error;
 
             try {
-                const database = new MemoryDatabaseService<IFolderModel>();
-                const repository = new FolderRepository(database);
-                const service = new FolderService(repository);
+                const database = new MemoryDatabaseService<IFileModel>();
+                const repository = new FileRepository(database);
+                const service = new FileService(repository);
 
-                instance = new FolderController(service);
+                instance = new FileController(service);
             } catch (e) {
                 instance = e as Error;
             }
 
-            assert.equal(instance instanceof FolderController, true);
+            assert.equal(instance instanceof FileController, true);
         });
     });
 
-    describe('FolderController handlers', () => {
-        let controller: FolderController;
+    describe('FileController handlers', () => {
+        let controller: FileController;
 
         beforeEach(async () => {
-            const database = new MemoryDatabaseService<IFolderModel>();
+            const database = new MemoryDatabaseService<IFileModel>();
             await database.connection.open();
-            const repository = new FolderRepository(database);
-            const service = new FolderService(repository);
+            const repository = new FileRepository(database);
+            const service = new FileService(repository);
 
-            controller = new FolderController(service);
+            controller = new FileController(service);
         });
 
-        describe('FolderController create', () => {
+        describe('FileController create', () => {
             it('should throw a bad request error if name is not provided', async () => {
-                let response: IFolderAPI | BadRequestResponse;
+                let response: IFileAPI | BadRequestResponse;
 
                 try {
                     response = await controller.create({ body: { } } as any, {} as any);
@@ -59,68 +59,38 @@ describe('FolderController', () => {
                 assert.equal(replied.code, 400);
                 assert.equal(replied.response, "Bad request: must have required property 'name'");
             });
-
-            it('should throw an error if parent folder is not found', async () => {
-                let response: IFolderAPI | NotFoundResponse;
-
-                try {
-                    response = await controller.create({ body: { name: 'test', description: 'test', parentIndex: 'parent' } } as any, { user: { sub: 1234 } } as any);
-                } catch (e) {
-                    response = e as NotFoundResponse;
-                }
-
-                const replied = (response as NotFoundResponse).reply() as { code: number, response: string };
-
-                assert.equal(response instanceof NotFoundResponse, true);
-                assert.equal(replied.code, 404);
-                assert.equal(replied.response, 'Not found: Parent folder not found');
-            });
-
-            it('should create a folder without parent', async () => {
-
-                const body = { name: 'test', description: 'test' };
-                const request = { body } as any;
-                const context = { user: { sub: 1234, name: '1234-test' } } as any;
-
-                const folder = await controller.create(request, context);
-
-                assert.equal(folder.name, 'test');
-                assert.equal(folder.description, 'test');
-                assert.equal(folder.parent, undefined);
-                assert.equal(folder.owner, '1234-test');
-            });
         });
 
-        describe('FolderController list', () => {
-            it('should return a list of folders', async () => {
+        describe('FileController list', () => {
+            it('should return a list of files', async () => {
                 const request = { params: {} } as any;
                 const context = { user: { sub: 1234, name: '1234-test' } } as any;
 
-                const folders = await controller.list(request, context);
+                const files = await controller.list(request, context);
 
-                assert.equal(folders.length, 0);
+                assert.equal(files.length, 0);
             });
 
-            it('should create a folder and return it', async () => {
-                const body = { name: 'test', description: 'test' };
+            it('should create a file and return it', async () => {
+                const body = { name: 'test', description: 'test', size: 1024, type: 'image/png' };
                 const request = { body } as any;
                 const context = { user: { sub: 1234, name: '1234-test' } } as any;
 
-                const folderCreated = await controller.create(request, context);
+                const fileCreated = await controller.create(request, context);
 
-                const folders = await controller.list(request, context);
-                const [ folderListed ] = folders;
+                const files = await controller.list(request, context);
+                const [ fileListed ] = files;
 
-                assert.equal(folders.length, 1);
-                assert.equal(folderListed.name, folderCreated.name);
-                assert.equal(folderListed.description, folderCreated.description);
-                assert.equal(folderListed.owner, '1234-test');
+                assert.equal(files.length, 1);
+                assert.equal(fileListed.name, fileCreated.name);
+                assert.equal(fileListed.description, fileCreated.description);
+                assert.equal(fileListed.owner, '1234-test');
             });
         });
 
-        describe('FolderController get', () => {
+        describe('FileController get', () => {
             it('should throw a bad request error if uuid is not provided', async () => {
-                let response: IFolderAPI | BadRequestResponse;
+                let response: IFileAPI | BadRequestResponse;
 
                 try {
                     response = await controller.get({ params: {} } as any, { user: { sub: 1234 } } as any);
@@ -135,8 +105,8 @@ describe('FolderController', () => {
                 assert.equal(replied.response, "Bad request: must have required property 'uuid'");
             });
 
-            it('should throw an error if folder is not found', async () => {
-                let response: IFolderAPI | NotFoundResponse;
+            it('should throw an error if file is not found', async () => {
+                let response: IFileAPI | NotFoundResponse;
 
                 try {
                     response = await controller.get({ params: { uuid: 'test' } } as any, { user: { sub: 1234 } } as any);
@@ -151,23 +121,23 @@ describe('FolderController', () => {
                 assert.equal(replied.response, 'Not found: Record not found');
             });
 
-            it('should get a folder', async () => {
-                const body = { name: 'test', description: 'test' };
+            it('should get a file', async () => {
+                const body = { name: 'test', description: 'test', size: 1024, type: 'image/png' };
                 const request = { body } as any;
                 const context = { user: { sub: 1234, name: '1234-test' } } as any;
 
-                const folderCreated = await controller.create(request, context);
-                const folder = await controller.get({ params: { uuid: folderCreated.uuid } } as any, context);
+                const fileCreated = await controller.create(request, context);
+                const file = await controller.get({ params: { uuid: fileCreated.uuid } } as any, context);
 
-                assert.equal(folder.name, folderCreated.name);
-                assert.equal(folder.description, folderCreated.description);
-                assert.equal(folder.owner, '1234-test');
+                assert.equal(file.name, fileCreated.name);
+                assert.equal(file.description, fileCreated.description);
+                assert.equal(file.owner, '1234-test');
             });
         });
 
-        describe('FolderController update', () => {
+        describe('FileController update', () => {
             it('should throw a bad request error if uuid is not provided', async () => {
-                let response: IFolderAPI | BadRequestResponse;
+                let response: IFileAPI | BadRequestResponse;
 
                 const request = { params: { uuid: undefined }, body: { } } as any;
                 const context = { user: { sub: 1234, name: '1234-test' } } as any;
@@ -184,8 +154,8 @@ describe('FolderController', () => {
                 assert.equal(replied.response, "Bad request: must have required property 'uuid'");
             });
 
-            it('should throw an error if folder is not found', async () => {
-                let response: IFolderAPI | NotFoundResponse;
+            it('should throw an error if file is not found', async () => {
+                let response: IFileAPI | NotFoundResponse;
 
                 const request = { params: { uuid: 'test' }, body: { } } as any;
                 const context = { user: { sub: 1234, name: '1234-test' } } as any;
@@ -203,23 +173,23 @@ describe('FolderController', () => {
                 assert.equal(replied.response, 'Not found: Record not found');
             });
 
-            it('should update a folder', async () => {
-                const body = { name: 'test', description: 'test' };
+            it('should update a file', async () => {
+                const body = { name: 'test', description: 'test', size: 1024, type: 'image/png' };
                 const request = { body } as any;
                 const context = { user: { sub: 1234, name: '1234-test' } } as any;
 
-                const folderCreated = await controller.create(request, context);
-                const folder = await controller.update({ params: { uuid: folderCreated.uuid }, ...request }, context);
+                const fileCreated = await controller.create(request, context);
+                const file = await controller.update({ params: { uuid: fileCreated.uuid }, ...request }, context);
 
-                assert.equal(folder.name, folderCreated.name);
-                assert.equal(folder.description, folderCreated.description);
-                assert.equal(folder.owner, '1234-test');
+                assert.equal(file.name, fileCreated.name);
+                assert.equal(file.description, fileCreated.description);
+                assert.equal(file.owner, '1234-test');
             });
         });
 
-        describe('FolderController delete', () => {
+        describe('FileController delete', () => {
             it('should throw a bad request error if uuid is not provided', async () => {
-                let response: IFolderAPI | BadRequestResponse;
+                let response: IFileAPI | BadRequestResponse;
 
                 try {
                     response = await controller.delete({ params: {} } as any, { user: { sub: 1234 } } as any);
@@ -234,8 +204,8 @@ describe('FolderController', () => {
                 assert.equal(replied.response, "Bad request: must have required property 'uuid'");
             });
 
-            it('should throw an error if folder is not found', async () => {
-                let response: IFolderAPI | NotFoundResponse;
+            it('should throw an error if file is not found', async () => {
+                let response: IFileAPI | NotFoundResponse;
 
                 const request = { params: { uuid: 'test' } } as any;
                 const context = { user: { sub: 1234, name: '1234-test' } } as any;
@@ -253,17 +223,17 @@ describe('FolderController', () => {
                 assert.equal(replied.response, 'Not found: Record not found');
             });
 
-            it('should delete a folder', async () => {
-                const body = { name: 'test', description: 'test' };
+            it('should delete a file', async () => {
+                const body = { name: 'test', description: 'test', size: 1024, type: 'image/png' };
                 const request = { body } as any;
                 const context = { user: { sub: 1234, name: '1234-test' } } as any;
 
-                const folderCreated = await controller.create(request, context);
-                const folder = await controller.delete({ params: { uuid: folderCreated.uuid } } as any, context);
+                const fileCreated = await controller.create(request, context);
+                const file = await controller.delete({ params: { uuid: fileCreated.uuid } } as any, context);
 
-                assert.equal(folder.name, folderCreated.name);
-                assert.equal(folder.description, folderCreated.description);
-                assert.equal(folder.owner, '1234-test');
+                assert.equal(file.name, fileCreated.name);
+                assert.equal(file.description, fileCreated.description);
+                assert.equal(file.owner, '1234-test');
             });
         });
     });
