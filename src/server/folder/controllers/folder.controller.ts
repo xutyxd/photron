dcbf103695695151a3c2f867114e6c9ed293bfc1
Xuty <xutyxd@gmail.com>
2024-10-07 01:17:27 +0200

    fix(folder): remove old imports

--- src/server/folder/controllers/folder.controller.ts
+++ src/server/folder/controllers/folder.controller.ts
@@ -1,24 +1,18 @@
-import { Ajv } from "ajv";
 import { inject, injectable } from "inversify";
-import { HttpMethodEnum, HTTPRequest, IHTTPContextData, IHTTPController, IHTTPControllerHandler } from "server-over-express";
+import { IHTTPController } from "server-over-express";
 import { EntityController } from "../../crosscutting/common";
-import { BaseError, NotFoundError } from "../../crosscutting/common/errors";
-import { BadRequestResponse, InternalErrorResponse, NotFoundResponse } from "../../crosscutting/common/responses";
-import { idRequest } from "../../crosscutting/common/schemas";
 import { FolderAPI } from "../classes/folder-api.class";
 import { IFolderAPIData, IFolderData, IFolderModelData } from "../interfaces/data";
-import { IFolderAPI } from "../interfaces/dto";
+import { IFolderAPIStatic } from "../interfaces/static";
 import { folderBase, folderCreate, folderUpdate } from "../schemas";
 import { FolderService } from "../services/folder.service";
-import { PartialFolder } from "../types/partial-folder.type";
-import { IFolderAPIStatic } from "../interfaces/static";
 
 @injectable()
 export class FolderController extends EntityController<IFolderAPIData, IFolderData, IFolderModelData, IFolderAPIStatic> implements IHTTPController {
 
     public path = 'folders';
 
-    constructor(@inject(FolderService) private readonly folderService: FolderService) {
+    constructor(@inject(FolderService) readonly folderService: FolderService) {
         const schemas = {
             base: folderBase,
             create: folderCreate,
