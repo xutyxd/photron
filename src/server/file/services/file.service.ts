b31cbb0f4568b2f4942f96a4753a2a763d7b2a43
Xuty <xutyxd@gmail.com>
2024-10-17 19:53:58 +0200

    feat(files): added owner uuid on file creation

--- src/server/file/services/file.service.ts
+++ src/server/file/services/file.service.ts
@@ -3,11 +3,18 @@ import { EntityService } from "../../crosscutting/common/services";
 import { File } from "../classes/file.class";
 import { IFileAPIData, IFileData, IFileModelData } from "../interfaces/data";
 import { FileRepository } from "../repository/file.repository";
+import { IHTTPContextData } from "server-over-express";
+import { IAuth } from "../../auth/interfaces/auth.interface";
 
 @injectable()
 export class FileService extends EntityService<IFileAPIData, IFileData, IFileModelData> {
 
     constructor(@inject(FileRepository) readonly fileRepository: FileRepository) {
         super(fileRepository, File);
+    }
+
+    public async create(data: IFileData, context: IHTTPContextData) {
+        const user = context.user as IAuth;
+        return await super.create({ ...data, ownerIndex: user.uuid });
     }
 }
