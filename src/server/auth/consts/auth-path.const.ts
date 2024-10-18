38a17af11b667e1416f011fd082f266b1b5ff7d3
Xuty <xutyxd@gmail.com>
2024-10-19 01:14:11 +0200

    fix(auth): updated auth paths to handle /directories

--- src/server/auth/consts/auth-path.const.ts
+++ src/server/auth/consts/auth-path.const.ts
@@ -3,6 +3,7 @@ import { IHTTPIntermediateAction } from "server-over-express";
 export const authPaths: IHTTPIntermediateAction['paths'] = {
     include: [
         '/auth/status',
+        '/directories',
         '/files',
         '/folders',
         '/photos',
